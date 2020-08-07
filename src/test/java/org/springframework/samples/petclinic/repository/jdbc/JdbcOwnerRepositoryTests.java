372de3e7959242c2a135d75d9639bbe4ca7ce1b2
Rajee4062 <32021313+Rajee4062@users.noreply.github.com>
2020-08-07 11:28:40 +0530

    Add files via upload

--- src/test/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryTests.java
+++ src/test/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryTests.java
@@ -1,25 +1,40 @@
 package org.springframework.samples.petclinic.repository.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.BDDMockito.given;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Map;
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.model.Owner;
 
-//@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 public class JdbcOwnerRepositoryTests 
 {
 	@Mock 
@@ -31,6 +46,10 @@ public class JdbcOwnerRepositoryTests
 	//@Autowired
 	JdbcOwnerRepositoryImpl  jdbcOwnerRepositoryImpl;
 	
+	private MockMvc mockMvc;
+	
+	@Autowired
+    protected ClinicService clinicService;
 	
 	DataSource dataSource;
 	//@Autowired
@@ -78,9 +97,19 @@ public class JdbcOwnerRepositoryTests
 	}
 	
 	
-	/*@Test
+	@Test
 	public void loadPetsAndVisits()
 	{
-		
-	}*/
+		  JdbcOwnerRepositoryImpl jdbcOwnerDao = new JdbcOwnerRepositoryImpl(dataSource);
+		  Owner owners = jdbcOwnerDao.findById(1);
+		 // System.out.println(owners);
+		  owners.setFirstName("Philip");
+		  owners.setCity("Hyderabad");
+		 // System.out.println(owners);
+		  jdbcOwnerDao.save(owners);
+		  Owner owners1 = jdbcOwnerDao.findById(1);
+        //  System.out.println(owners1);
+		  assertEquals("Philip",owners1.getFirstName());
+		 
+	}
 }
---
+++ src/test/java/org/springframework/samples/petclinic/repository/jdbc/JdbcPetRepositoryImplTest.java
@@ -0,0 +1,59 @@
+package org.springframework.samples.petclinic.repository.jdbc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.repository.OwnerRepository;
+
+
+public class JdbcPetRepositoryImplTest 
+{
+	DataSource dataSource;
+	
+	@Autowired
+	OwnerRepository ownerRepository;
+	
+	@BeforeEach
+	void setUp()
+	{
+	   dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
+			      .addScript("classpath:db/hsqldb/initDB.sql")
+			      .addScript("classpath:db/hsqldb/populateDB.sql")
+			      .build();
+	}
+	
+	@Test
+	public void findPetTypesTest() throws Exception
+	{
+		List<PetType> list = new ArrayList<PetType>();
+		JdbcPetRepositoryImpl jdbcPetDao = new JdbcPetRepositoryImpl(dataSource,ownerRepository);
+		list = (List<PetType>) jdbcPetDao.findPetTypes();
+		//System.out.println(list);
+		assertEquals(6,list.size());
+	}
+	
+	@Test
+	public void findByIdTest()
+	{
+	    Pet p = new Pet();
+		//List<Pet> list = new ArrayList<Pet>();
+		JdbcPetRepositoryImpl jdbcPetDao = new JdbcPetRepositoryImpl(dataSource,ownerRepository);
+	     p = jdbcPetDao.findById(1);
+		System.out.println(jdbcPetDao.findById(1)+"    hh       "+p);
+		//assertEquals(6,list.size());
+	}
+	
+	}
