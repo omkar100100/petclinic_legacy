cd05b6c6776ae4193a5f91d50073633c680f9a53
Yash Gupta <54623282+yashguptamphasis@users.noreply.github.com>
2020-08-10 13:45:48 +0530

    Update TestVetJpa.java
    With the previous change, the dependency problem is resolved, all working good.

--- src/test/java/org/springframework/samples/petclinic/repository/jpa/TestVetJpa.java
+++ src/test/java/org/springframework/samples/petclinic/repository/jpa/TestVetJpa.java
@@ -1,14 +1,17 @@
 package org.springframework.samples.petclinic.repository.jpa;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.transaction.Transactional;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Visit;
@@ -16,15 +19,14 @@ import org.springframework.samples.petclinic.repository.VetRepository;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(locations = 
-{"classpath:spring/business-config.xml",
-		"classpath:spring/mvc-test-config.xml"})
-@ActiveProfiles("jpa")
+//@RunWith(SpringRunner.class)
+@SpringJUnitConfig(locations= {"classpath:spring/business-config.xml","classpath:spring/mvc-test-config.xml"})
+@ActiveProfiles({"jpa","HSQLDB"})
 public class TestJpaVetRepo {
 
 	@Autowired
